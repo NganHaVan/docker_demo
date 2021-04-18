ed07d0c77f1280ad4fef4b84390ec3f493d9f42e
Janet Van <ha.van@reactron.dev>
2021-04-18 16:44:53 +0300

    Fix auto uploading after code change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Test Netlify</h1>
+        <h1>Hello HaVan voila</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Start learn react
+          Master of React 23
         </a>
       </header>
     </div>
